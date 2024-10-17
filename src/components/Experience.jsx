03bdfbde9279caf6c16eee78c30516002f649b55
Abhishek Swaroop <103449329+abhiswrp@users.noreply.github.com>
2024-10-18 02:05:31 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -37,9 +37,6 @@ const Experience = () => {
               {experience.technologies.map((tech,index)=>(
                 <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
                   {tech}
-                  <br></br>
-                  <br></br>
-                  <br></br>
                 </span>
               ))}
             </motion.div>
