f08233b525bc3a0372508664f44543c651621a78
Abhishek Swaroop <abhi.swrp4321@gmail.com>
2024-12-29 02:08:48 +0530

    A bit of modifications

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,106 +5,104 @@ import project3 from "../assets/projects/Hal_Jivi.jpg";
 import project4 from "../assets/projects/GunGuardian_1.jpg";
 import project5 from "../assets/projects/Preserve Your Heart.jpg";
 // export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
-export const HERO_CONTENT = 'As a passionate full stack developer with 1 year of hands-on experience, I specialize in crafting robust and scalable web applications. I have developed strong skills in front-end technologies like React, and back-end technologies such as Node.js, MySQL, and MongoDB. Additionally, I have experience in Blockchain, Web3, and Machine Learning. My goal is to leverage my growing expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.';
+export const HERO_CONTENT = 'I am a passionate front-end developer and freelancer with over a year of experience, specializing in building dynamic, user-friendly web interfaces using React. My expertise lies in creating visually appealing and responsive designs that elevate user experiences. As a freelancer, I have successfully delivered diverse projects, collaborating with clients to translate their ideas into impactful digital solutions. With additional skills in Blockchain, Web3, and Machine Learning, I bring innovation and creativity to every project, ensuring high-quality results that meet and exceed expectations.';
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With an year of professional experience, I have worked with a variety of technologies and participated in a National Level Hack-A-Thon. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-export const EXPERIENCES = [
-  {
-    year: "2023 - 2023",
-    role: "Android Developer Intern",
-    company: "CodSoft",
-    link:"https://drive.google.com/file/d/11-DnXpFbMM7-YH9zoytyMDcuKKZhCLU5/view",
-    description: `Developed high-performance Android apps with Material Design, incorporating features like authentication and real-time updates. Achieved a 25% boost in user engagement, 30% fewer bugs, and 20% faster feature delivery. Utilized Git, Jira, and Android Studio for project management and collaboration.`,
-    technologies: ["Java", "Android Studio", "Kotlin", "Git", "Google APIs"],
-  },
-  {
-    year: "2024 - 2024",
-    role: "Software Development Engineer (SDE) Intern",
-    company: "Bluestack Fintech",
-    link:"https://drive.google.com/file/d/1TnILvIxJEV9L_1gKYp_SDLLR4LuV3Yzw/view?usp=drive_link",
-    description: `A software developer intern gains foundational experience by working with front-end technologies (HTML, CSS, JavaScript, React.js), back-end frameworks (Node.js, Express.js, Python), and databases (SQL, NoSQL), while utilizing version control (Git), development tools (VS Code, Postman), and deployment platforms (Heroku), all while enhancing problem-solving skills, communication, and adaptability.`,
-    technologies: ["HTML", "CSS", "JS", "Python", "SQL", "Git", "AWS"],
-  },
+export const EXPERIENCES = [{
+        year: "2023 - 2023",
+        role: "Android Developer Intern",
+        company: "CodSoft",
+        link: "https://drive.google.com/file/d/11-DnXpFbMM7-YH9zoytyMDcuKKZhCLU5/view",
+        description: `Developed high-performance Android apps with Material Design, incorporating features like authentication and real-time updates. Achieved a 25% boost in user engagement, 30% fewer bugs, and 20% faster feature delivery. Utilized Git, Jira, and Android Studio for project management and collaboration.`,
+        technologies: ["Java", "Android Studio", "Kotlin", "Git", "Google APIs"],
+    },
+    {
+        year: "2024 - 2024",
+        role: "Software Development Engineer (SDE) Intern",
+        company: "Bluestack Fintech",
+        link: "https://drive.google.com/file/d/1TnILvIxJEV9L_1gKYp_SDLLR4LuV3Yzw/view?usp=drive_link",
+        description: `A software developer intern gains foundational experience by working with front-end technologies (HTML, CSS, JavaScript, React.js), back-end frameworks (Node.js, Express.js, Python), and databases (SQL, NoSQL), while utilizing version control (Git), development tools (VS Code, Postman), and deployment platforms (Heroku), all while enhancing problem-solving skills, communication, and adaptability.`,
+        technologies: ["HTML", "CSS", "JS", "Python", "SQL", "Git", "AWS"],
+    },
 ];
 
-export const PROJECTS = [
-  {
-    title: "Web 3.0 Transact",
-    image: project1,
-    // link:"https://github.com/abhiswrp/Web-3.0-Transact",
-    link:"https://swaroop-s-transact.vercel.app",
-    description:"A Web 3.0 application using Solidity and MetaMask, showcasing decentralized technology principles. This project serves as a practical example of building a dApp, highlighting innovative Web 3.0 methodologies to enhance user engagement and security.",
-    technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Ethereum", "MetaMask"],
-  },
-  /*
-  {
-    title: "RealEstate Marketplace",
-    image: project2,
-    link:"https://github.com/abhiswrp/RealEstate-Marketplace",
-    description:"A Blockchain based Web3 platform that enables secure and transparent property transactions. Designed to support up to 5,000 users, the project aims to streamline the buying and selling process while reducing transaction costs by 30% through decentralized ledger technology.",
-    technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Polygon", "mySQL"],
-  },
-  */
-  {
-    title: "Swarooop's Portfolio",
-    image: project2,
-    // link:"https://github.com/abhiswrp/Swaroop-s_Portfolio",
-    link:"https://swaroop-s-portfolio.vercel.app",
-    description:"A dynamic and visually appealing portfolio showcasing my projects and skills as a Full Stack Developer. Built using React for efficient and responsive UI, TailwindCSS for modern and customizable styling, and FramerMotion for smooth animations and interactive transitions. The portfolio demonstrates my expertise in creating seamless user experiences and is optimized for performance and accessibility.",
-    technologies: ["React", "TailWindCSS", "Vite", "Vercel"],
-  },
-  {
-    title: "Hal-Jivi",
-    image: project3,
-    link:"https://github.com/abhiswrp/Hal-Jivi",
-    description:"A Blockchain-based Web3 platform for the agricultural sector, Hal Jivi enables secure and transparent Minimum Support Price (MSP) calculations for farmers. Designed to support a vast number of agricultural stakeholders, the project aims to streamline the MSP calculation process while reducing pricing discrepancies by 30% through region-specific indexing and decentralized ledger technology.",
-    technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Polygon"],
-  },
-  {
-    title: "Gun Guardian",
-    image: project4,
-    link:"https://github.com/abhiswrp/GunGuardian",
-    description:"A Python application that detects firearms and sends notifications to registered users, designed to enhance safety awareness and communication. The project is poised for deployment, with plans to reach an estimated 1,000 users upon launch.",
-    technologies: ["AWS", "OpenCV", "Python", "Machine Learning", "YOLOv8"],
-  },
-  {
-    title: "Preserve Your Heart",
-    image: project5,
-    link:"https://github.com/abhiswrp/Preserve-Your-Heart",
-    description:"A Machine Learning WebApp that leverages predictive analytics to enhance cardiovascular health outcomes by identifying at-risk patients with 85% accuracy, reducing hospital readmission rates by 30% through data-driven insights.",
-    technologies: ["JupyterLab", "Google Colab", "Python", "Machine Learning"],
-  },
-  /*
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
-  */
+export const PROJECTS = [{
+        title: "Web 3.0 Transact",
+        image: project1,
+        // link:"https://github.com/abhiswrp/Web-3.0-Transact",
+        link: "https://swaroop-s-transact.vercel.app",
+        description: "A Web 3.0 application using Solidity and MetaMask, showcasing decentralized technology principles. This project serves as a practical example of building a dApp, highlighting innovative Web 3.0 methodologies to enhance user engagement and security.",
+        technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Ethereum", "MetaMask"],
+    },
+    /*
+    {
+      title: "RealEstate Marketplace",
+      image: project2,
+      link:"https://github.com/abhiswrp/RealEstate-Marketplace",
+      description:"A Blockchain based Web3 platform that enables secure and transparent property transactions. Designed to support up to 5,000 users, the project aims to streamline the buying and selling process while reducing transaction costs by 30% through decentralized ledger technology.",
+      technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Polygon", "mySQL"],
+    },
+    */
+    {
+        title: "Swarooop's Portfolio",
+        image: project2,
+        // link:"https://github.com/abhiswrp/Swaroop-s_Portfolio",
+        link: "https://swaroop-s-portfolio.vercel.app",
+        description: "A dynamic and visually appealing portfolio showcasing my projects and skills as a Full Stack Developer. Built using React for efficient and responsive UI, TailwindCSS for modern and customizable styling, and FramerMotion for smooth animations and interactive transitions. The portfolio demonstrates my expertise in creating seamless user experiences and is optimized for performance and accessibility.",
+        technologies: ["React", "TailWindCSS", "Vite", "Vercel"],
+    },
+    {
+        title: "Hal-Jivi",
+        image: project3,
+        link: "https://github.com/abhiswrp/Hal-Jivi",
+        description: "A Blockchain-based Web3 platform for the agricultural sector, Hal Jivi enables secure and transparent Minimum Support Price (MSP) calculations for farmers. Designed to support a vast number of agricultural stakeholders, the project aims to streamline the MSP calculation process while reducing pricing discrepancies by 30% through region-specific indexing and decentralized ledger technology.",
+        technologies: ["Blockchain", "Web 3.0", "React", "Solidity", "Polygon"],
+    },
+    {
+        title: "Gun Guardian",
+        image: project4,
+        link: "https://github.com/abhiswrp/GunGuardian",
+        description: "A Python application that detects firearms and sends notifications to registered users, designed to enhance safety awareness and communication. The project is poised for deployment, with plans to reach an estimated 1,000 users upon launch.",
+        technologies: ["AWS", "OpenCV", "Python", "Machine Learning", "YOLOv8"],
+    },
+    {
+        title: "Preserve Your Heart",
+        image: project5,
+        link: "https://github.com/abhiswrp/Preserve-Your-Heart",
+        description: "A Machine Learning WebApp that leverages predictive analytics to enhance cardiovascular health outcomes by identifying at-risk patients with 85% accuracy, reducing hospital readmission rates by 30% through data-driven insights.",
+        technologies: ["JupyterLab", "Google Colab", "Python", "Machine Learning"],
+    },
+    /*
+    {
+      title: "E-Commerce Website",
+      image: project1,
+      description:
+        "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    },
+    {
+      title: "Task Management App",
+      image: project2,
+      description:
+        "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+      technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    },
+    {
+      title: "Portfolio Website",
+      image: project3,
+      description:
+        "A personal portfolio website showcasing projects, skills, and contact information.",
+      technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    },
+    {
+      title: "Blogging Platform",
+      image: project4,
+      description:
+        "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+      technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    },
+    */
 ];
 export const CONTACT = {
-  phoneNo: "+91 9555087023",
-  email: "abhi.swrp4321@gmail.com",
+    phoneNo: "+91 9555087023",
+    email: "abhi.swrp4321@gmail.com",
 };
