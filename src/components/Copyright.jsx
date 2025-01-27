791fe8f597a8102d64d9ad3d29eb72de781c1703
Abhishek Swaroop <abhi.swrp4321@gmail.com>
2025-01-28 01:25:16 +0530

    Coppyright - Updated

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -5,28 +5,28 @@ const Copyright = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className="py-8 bg-neutral-950 text-center border-t border-neutral-800">
+        <div className="py-10 bg-neutral-950 text-center border-t border-neutral-800">
             <motion.div
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: 20 }}
                 transition={{ duration: 0.5 }}
-                className="text-neutral-300 flex flex-col items-center gap-6 lg:gap-12"
+                className="text-neutral-300 flex flex-col items-center"
             >
                 <img
                     src={emblem}
                     alt="Emblem"
-                    className="w-40 h-auto lg:w-96 lg:h-auto"
+                    className="w-35 h-35 lg:w-48 lg:h-auto transition-transform transform hover:scale-110"
                 />
-                <div className="flex flex-col lg:flex-row items-center lg:justify-between gap-8 lg:gap-12 text-sm lg:text-base">
-                    <p className="text-neutral-400">
+                <div className="flex flex-col lg:flex-row items-center lg:justify-between gap-3 lg:gap-5 text-sm lg:text-base">
+                    <h1 className="text-neutral-400 font-medium hover:text-purple-400 transition-colors">
                         © {currentYear} Abhishek Swaroop. All rights reserved.
-                    </p>
-                    <p className="text-neutral-400">
-                        Designed & Developed using React, TailwindCSS and Framer Motion.
-                    </p>
-                    <p className="text-neutral-400">
+                    </h1>
+                    <h1 className="text-neutral-400 text-xs lg:text-sm font-light">
+                        Designed & Developed using React, TailwindCSS, and Framer Motion.
+                    </h1>
+                    <h1 className="text-neutral-400 text-xs lg:text-sm font-light">
                         Passionate about creating impactful web experiences.
-                    </p>
+                    </h1>
                 </div>
             </motion.div>
         </div>
